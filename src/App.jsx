6d325892dce0e83b497a6145d68ccac3d29b768a
willams <willamshenrique070@gmail.com>
2025-04-15 07:18:17 -0300

    fix: correct todo list functionality

--- src/App.jsx
+++ src/App.jsx
@@ -30,45 +30,50 @@ const [todos, setTodos] = useState([
 ]);
 
 const [search, setSearch] = useState("");
-
 const [filter, setFilter] = useState("All");
-const [sort, setSort] = useState("Asc");
 
 const addTodo = (text, category) => {
   const newTodos = [
-    ...todos,{
-    id: Math.floor(Math.random() * 10000),
-    text,
-    category,
-    isCompleted: false,
-  },
-];
+    ...todos,
+    {
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false,
+    }
+  ];
   setTodos(newTodos);
 };
 
 const removeTodo = (id) => {
-  const newTodos = [...todos]
-  const filteredTodos = newTodos.filter((todo) => 
-    todo.id !== id ? todo : null
-  );
+  const filteredTodos = todos.filter((todo) => todo.id !== id);
   setTodos(filteredTodos);
 };
 
 const completeTodo = (id) => {
-  const newTodos = [...todos]
-  newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
+  const newTodos = todos.map((todo) => 
+    todo.id === id ? { ...todo, isCompleted: !todo.isCompleted } : todo
+  );
   setTodos(newTodos);
-}
+};
 
   return (
    <div className='app'>
-    <h1> Lista de Tarefas</h1>
+    <h1>Lista de Tarefas</h1>
     <Search search={search} setSearch={setSearch}/>
     <Filter filter={filter} setFilter={setFilter}/>
     <div className="todo-list">
       {todos
-        .filter((todo) => filter === "All" ? true : filter === "Completed" ? todo.isCompleted : !todo.isCompleted)
-        .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
+        .filter((todo) => 
+          filter === "All" 
+            ? true 
+            : filter === "Completed" 
+              ? todo.isCompleted 
+              : !todo.isCompleted
+        )
+        .filter((todo) => 
+          todo.text.toLowerCase().includes(search.toLowerCase())
+        )
         .map((todo) => (
           <Todo
             key={todo.id}
@@ -77,11 +82,10 @@ const completeTodo = (id) => {
             completeTodo={completeTodo}
           />
         ))}
-      </div>
-      <TodoForm addTodo={addTodo}/>
-      </div>
-  )
-  }
+    </div>
+    <TodoForm addTodo={addTodo}/>
+   </div>
+  );
+}
 
-
-  export default App
+export default App;
